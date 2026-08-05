0e4c4f0988b088455f2f75a6b661b668a880c9d3
Omar Gutierrez <omandres2505@gmail.com>
2026-08-05 15:41:43 -0500

    feat(admin): add services management
    Migrates _data/services.yml (name, description, icon) to a services
    table with full CRUD.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SiteSettingSeeder::class,
             NavItemSeeder::class,
+            ServiceSeeder::class,
         ]);
     }
 }
